0ee108e0834898b1ad9354721de7cd6dee2769d6
Yuma-alt <pjp68792@gmail.com>
2024-01-21 01:02:11 +0900

    Initial Commit

---
+++ gpsmemo/gpsmemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  gpsmemoApp.swift
+//  gpsmemo
+//
+//  Created by Yuma on 2024/01/21.
+//
+
+import SwiftUI
+
+@main
+struct gpsmemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
