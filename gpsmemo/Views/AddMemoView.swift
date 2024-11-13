483021b79d285e9ced572bd7018580632c59a67c
Yuma-alt <pjp68792@gmail.com>
2024-11-13 21:47:31 +0900

    delete latitude and longitude

--- gpsmemo/Views/AddMemoView.swift
+++ gpsmemo/Views/AddMemoView.swift
@@ -35,7 +35,6 @@ struct AddMemoView: View {
                     
                     if let location = memo.location {
                         Section(header: Text("Location")) {
-                            Text("Latitude: \(location.latitude), Longitude: \(location.longitude)")
                             if let address = viewModel.currentAddress {
                                 Text("Address: \(address)")
                             }
